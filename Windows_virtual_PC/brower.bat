ab32fbcee2ed3b6e8a5da84675e86ba351c149b9
johnny20061234 <46470547+congaterori@users.noreply.github.com>
2020-11-21 15:31:03 +0700

    Update brower.bat

--- Windows_virtual_PC/brower.bat
+++ Windows_virtual_PC/brower.bat
@@ -3,23 +3,36 @@
 title browser
 color 1f
 cls
-type C:\Users\%USERNAME%\Desktop\Windows_virtual_PC\board.txt
+echo .-------------------------------------,
+echo ^|   1.search                          ^|
+echo ^|   2.download load page              ^|
+echo ^|   3.download picture web page       ^|
+echo ^|   4.download file                   ^|
+echo ^|   5.telnet                          ^|
+echo '-------------------------------------'
 echo.
 set /p input=choose: 
 if %input% == 1 goto search
 if %input% == 2 goto downloadpage
 if %input% == 3 goto downloadpicture
 if %input% == 4 goto download
+if %input% == 5 goto telnet
 goto main
 :search
 cls
 set /p search=Search website or enter a url: 
-cmdMenuSel f971 "Search" "I'm Feeling Lucky" "back"
+:donesearch
+cls
+echo Search website or enter a url: %search%
+cmdMenuSel f971 "Search" "I'm Feeling Lucky" "back" "main page" "Images" "Video" "News" "Places"
 if %ERRORLEVEL% == 1 goto searchongoogle
 if %ERRORLEVEL% == 2 goto i'mfeelinglucky
-if %ERRORLEVEL% == 3 goto main
-cls
-goto search
+if %ERRORLEVEL% == 3 goto search
+if %ERRORLEVEL% == 4 goto main
+if %ERRORLEVEL% == 5 goto image
+if %ERRORLEVEL% == 6 goto video
+if %ERRORLEVEL% == 7 goto place
+goto donesearch
 :searchongoogle
 cls
 setlocal enabledelayedexpansion
@@ -27,7 +40,7 @@ set url=!search: =+!
 start https://www.google.com/search?q=%url%
 pause
 END local
-goto main
+goto donesearch
 :i'mfeelinglucky
 cls
 SETLOCAL ENABLEDELAYEDEXPANSION
@@ -35,7 +48,39 @@ set url2=!search: =+!
 explorer.exe "https://www.google.com/search?q=%url2%&btnI"
 pause
 END local
-goto main
+goto donesearch
+:image
+cls
+setlocal enabledelayedexpansion
+set url=!search: =+!
+explorer.exe "https://www.google.com/search?q=%url%&tbm=isch"
+pause
+END local
+goto donesearch
+:video
+cls
+setlocal enabledelayedexpansion
+set url=!search: =+!
+explorer.exe "https://www.google.com/search?q=%url%&tbm=vid"
+pause
+END local
+goto donesearch
+:news
+cls
+setlocal enabledelayedexpansion
+set url=!search: =+!
+explorer.exe "https://www.google.com/search?q=%url%&tbm=nws"
+pause
+END local
+goto donesearch
+:places
+cls
+setlocal enabledelayedexpansion
+set url=!search: =+!
+explorer.exe "https://www.google.com/search?q=%url%&tbm=plcs"
+pause
+END local
+goto donesearch
 :downloadpage
 cls
 set /p website=website: 
@@ -58,5 +103,11 @@ set /p filedownload=link file to download:
 set /p namedownload=name file: 
 powershell -Command "Invoke-WebRequest %filedownload% -OutFile %namedownload%"
 ::start %filedownload%
+pause
+goto main
+:telnet
+cls
+set /p telnetweb=telnet:
+start Telnet %telnetweb%
 pause
 goto main
