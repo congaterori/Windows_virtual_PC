3c3e01adac1d64a72d200d0eddbd971e725715ae
johnny20061234 <46470547+congaterori@users.noreply.github.com>
2022-01-23 18:36:34 +0700

    Add files via upload

---
+++ uninstall.bat
@@ -0,0 +1,23 @@
+@echo off
+title uninstall
+:loop
+cls
+echo do you want to uninstall all?(yes/no)
+set /p choose=
+if %chooose% == yes goto uninstall
+if %chooose% == no exit
+if %chooose% == Yes goto uninstall
+if %chooose% == No exit
+if %chooose% == YES goto uninstall
+if %chooose% == NO exit
+if %chooose% == y goto uninstall
+if %chooose% == n exit
+if %chooose% == Y goto uninstall
+if %chooose% == N exit
+goto loop
+:uninstall
+cls
+echo y | rmdir /S C:\Windowspc > nul
+echo y | rmdir /S C:\Windowspcbatchfilepro > nul
+start /min cmd /k "cd.. & echo y | rmdir /S Windows_virtual_PC-demo-pro & exit"
+exit
